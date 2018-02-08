160fe06850ce3be01ceb117bb3f949f1fc77b11d
unknown <pizzahan1213@163.com>
2018-02-08 17:22:44 +0800

    适配docker，配置由环境变量获取

--- crawler/downloaderdistribute/download_config.py
+++ crawler/downloaderdistribute/download_config.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import os
 import redis
-import configparser
 import logging
 import logging.config
 import unittest
@@ -11,27 +10,25 @@ from download_mysql import DbMysql
 
 
 class MdConfig(object):
-    def __init__(self, config):
-        cf = configparser.ConfigParser()
-        cf.read(config)
-
+    def __init__(self):
+        env_idc = os.environ
         # redis config
-        self.rds_host = cf.get('redis', 'host')
-        self.rds_port = cf.getint('redis', 'port')
-        self.rds_password = cf.get('redis', 'password')
+        self.rds_host = env_idc.get('redis_host')
+        self.rds_port = env_idc.getint('redis_port')
+        self.rds_password = env_idc.get('redis_password')
         self.rds = redis.Redis(host=self.rds_host, port=self.rds_port, db=0, password=self.rds_password,
                                decode_responses=True)
         logging.info('redis host[{0}] port[{1}]'.format(self.rds_host, self.rds_port))
 
         # stop key
-        self.run_seqs = cf.get('data', 'run_seqs')
-        self.stop_key = cf.get('data', 'stop_key')
-        self.data_path = cf.get('data', 'data_path')
-        self.stat_hash = cf.get('data', 'stat_hash')
-        self.proxy_key = cf.get('data', 'proxy_key')
-        self.proxy_index = cf.get('data', 'proxy_index')
-        self.lock_sesseion_sec = cf.get('data', 'lock_sesseion_sec')
-        self.time_out = cf.getint('data', 'time_out')
+        self.run_seqs = env_idc.get('download_run_seqs')
+        self.stop_key = env_idc.get('download_stop_key')
+        self.data_path = env_idc.get('download_data_path')
+        self.stat_hash = env_idc.get('download_stat_hash')
+        self.proxy_key = env_idc.get('download_proxy_key')
+        self.proxy_index = env_idc.get('download_proxy_index')
+        self.lock_sesseion_sec = env_idc.get('download_lock_sesseion_sec')
+        self.time_out = env_idc.getint('download_time_out')
 
         # proxy config
         self.proxy_host = self.rds.hget(self.proxy_key, 'host')
@@ -41,11 +38,11 @@ class MdConfig(object):
         logging.info('proxy host[{0}] port[{1}]'.format(self.proxy_host, self.proxy_port))
 
         # mysql config
-        self.my_host = cf.get('mysql', 'host')
-        self.my_port = cf.getint('mysql', 'port')
-        self.my_user = cf.get('mysql', 'user')
-        self.my_password = cf.get('mysql', 'password')
-        self.my_db = cf.get('mysql', 'db')
+        self.my_host = env_idc.get('mysql_host')
+        self.my_port = env_idc.getint('mysql_port')
+        self.my_user = env_idc.get('mysql_user')
+        self.my_password = env_idc.get('mysql_password')
+        self.my_db = env_idc.get('mysql_db')
         self.mdb = DbMysql(self.my_host, self.my_port, self.my_user, self.my_password, self.my_db)
         logging.info('mysql host[{0}] port[{1}]'.format(self.my_host, self.my_port))
 
@@ -63,7 +60,7 @@ class TestMdConfig(unittest.TestCase):
         log_conf = './conf/logging.conf'
         logging.config.fileConfig(log_conf)
 
-        md_config = MdConfig(conf_file)
+        md_config = MdConfig()
         rds = md_config.get_rds()
         print(rds.ping())
 
--- crawler/downloaderdistribute/gain_downloader_main.py
+++ crawler/downloaderdistribute/gain_downloader_main.py
@@ -14,7 +14,6 @@ from download_thread import MdThread
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config', help='配置文件', default='conf/downloader.conf')
     parser.add_argument('--logging', help='日志配置', default='conf/logging.conf')
     args = parser.parse_args()
 
@@ -22,7 +21,7 @@ def main():
         os.makedirs('log', exist_ok=True)
         logging.config.fileConfig(args.logging)
 
-    config = MdConfig(args.config)
+    config = MdConfig()
     abu_thread = MdThread(config)
     abu_thread.run()
     abu_thread.join()
